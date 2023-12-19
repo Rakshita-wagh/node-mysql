e0b54579ed684526964a4905f62b7aef82d7b61c
Rakshita-wagh <01fe21bcs280@kletech.ac.in>
2023-12-19 14:09:34 +0530

    table

--- table.js
+++ table.js
@@ -10,24 +10,32 @@ var con=mysql.createConnection(
 )
 
 
-con.connect((err)=>
-{
-  if(err) throw err;
-  console.log('connected');
-  
-  var sql1=  
-  `CREATE TABLE users(user_id INT(5) AUTO_INCREMENT PRIMARY KEY,user_name varchar(20),user_mail(40))`;
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("connected");
 
-  var sql2=
-  `CREATE TABLE posts(post_id INT(5) AUTO_INCREMENT PRIMARY KEY,title varchar(20),content(20),FOREIGN KEY (user_id) REFERENCES users(user_id),date varchar(20))`;
+    var sql1=  
+  `CREATE TABLE users(user_id INT(5) AUTO_INCREMENT PRIMARY KEY,user_name VARCHAR(20),user_mail VARCHAR(40))`;
+    con.query(sql1, function (err, result) {
+        if (err) throw err;
+        console.log("Table 'user' created");
+    });
 
+    var sql2=
+    `CREATE TABLE posts(post_id INT(5) AUTO_INCREMENT PRIMARY KEY,title VARCHAR(20),content VARCHAR(20),user_id INT(5),FOREIGN KEY (user_id) REFERENCES users(user_id),date VARCHAR(20))`;
+
+    con.query(sql2, function (err, result) {
+        if (err) throw err;
+        console.log("Table 'post' created");
+    });
+   
   var sql3=
-  `CREATE TABLE comments(comment_id INT(5) AUTO_INCREMENT PRIMARY KEY,FOREIGN KEY (post_id) REFERENCES posts(post_id)),FOREIGN KEY (user_id)REFERENCES users(user_id),comment varchar(20))`;
+  `CREATE TABLE comments(comment_id INT(5) AUTO_INCREMENT PRIMARY KEY,post_id INT(5),FOREIGN KEY (post_id) REFERENCES posts (post_id),user_id INT(5),FOREIGN KEY (user_id) REFERENCES users(user_id),comment VARCHAR(20))`;
 
-  con.query((sql1,sql2,sql3),(err)=>
-  {
-    if(err) throw err;
-    console.log("tables created");
-  }
-  )
+    con.query(sql3, function (err, result) {
+        if (err) throw err;
+        console.log("Table 'comment' created");
+    });
 });
+
+ 
